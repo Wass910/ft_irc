fa908d2e72b1653f9376af0c2cf0f590ff4791bb
Ilyes DHIBA <idhiba@e2r5p14.42.fr>
2022-05-05 18:11:31 +0200

    client communique avec server

--- client.cpp
+++ client.cpp
@@ -1,45 +1,34 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <iostream>
-#include <vector>
+#include "utils.hpp"
 
-typedef struct User{
-	char nom[30];
-	int age;
-}User;
+int main()
+{
+    int socketClient = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addrClient;
 
-int main(void){
-	int socketClient = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in addrClient;
-	addrClient.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addrClient.sin_family = AF_INET;
-	addrClient.sin_port = htons(30000);
-	connect(socketClient, (const struct sockaddr *)&addrClient, sizeof(addrClient));
-	std::cout << "connected." << std::endl;
+    addrClient.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addrClient.sin_family = AF_INET;
+    addrClient.sin_port = htons(30003);
 
-	char msg[500];
-	recv(socketClient, msg, 500, 0);
-	std::cout << msg << std::endl;
-	std::string temp;
-	getline(std::cin, temp);
-	int len = 500;
-	char msg_send[len];
-	int i = 0;
-	while(temp[i])
-	{
-		msg_send[i] = temp[i];
-		i++;
-	}
-	msg_send[i] = '\0';
-	send(socketClient, msg_send, len, 0);
-	close(socketClient);
+    connect(socketClient, (const struct sockaddr *)&addrClient, sizeof(addrClient));
+    std::cout << "Connecte" << std::endl;
 
-	return 0;
+
+
+    char msg[500];
+    recv(socketClient, &msg, 500, 0);
+    std::cout << msg << std::endl;
+    std::string temp;
+    User user;
+    while (1)
+    {
+        std::cout << "message to send : "; 
+        getline(std::cin, temp);
+        user.len = temp.size();
+        strcpy(user.msg, temp.c_str());
+        send(socketClient, &user, sizeof(User), 0);
+        temp.replace(0, temp.size(), "");
+    }
+
+    std::cout << "Done GG as well !" << std::endl;
+    return 0;
 }
