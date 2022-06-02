81dc6230e7e69127b31a0dae291a134a2a8aabcf
Ilyes DHIBA <idhiba@e3r9p18.clusters.42paris.fr>
2022-06-02 13:41:36 +0200

    almost done

--- main.cpp
+++ main.cpp
@@ -12,15 +12,36 @@ void	inthandler(int sig)
 	kill_server = 1;
 }
 
+bool	check_port(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return false;
+		i++;
+	}
+	return true;
+}
 int main(int argc, char **argv)
 {
+	if (argc < 3)
+	{
+		std::cout << "please enter a port and a password.\n";
+		exit(0);
+	}
+	if (check_port(argv[1]) == false)
+	{
+		std::cout << "Only numbers is accepted for the port\n";
+		exit(0);
+	}
 	Server server(std::atoi(argv[1]), argv[2]);
 	std::list<pollfd> temp_lfds = server.get_lfds();
 	std::list<pollfd>::iterator begin = temp_lfds.begin();
 
 	signal(SIGINT, inthandler);
 	port = std::atoi(argv[1]);
-	
     while(1 && kill_server == 0)
     {
 		temp_lfds = server.get_lfds();
